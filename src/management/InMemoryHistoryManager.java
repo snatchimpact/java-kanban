f4020f2976890f4e80558852ac0e675d10ec3970
snatchimpact <tedeev.tkw@yandex.ru>
2023-01-31 15:59:30 +0300

    Собрал нужные методы во внутр. класс CustomLinkedList. Спасибо!

--- src/management/InMemoryHistoryManager.java
+++ src/management/InMemoryHistoryManager.java
@@ -7,67 +7,78 @@ import java.util.HashMap;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    public Node<Task> head = null;
-    public Node<Task> tail = null;
+
     public HashMap<Integer, Node<Task>> tasksViewingHistory = new HashMap<>();
+    public CustomLinkedList customLinkedList = new CustomLinkedList();
 
-    public void linkLast(Task task){
-        Node<Task> newNode = new Node<>(task);
-        if(head == null){
-            head = tail = newNode;
-            head.previous = null;
-        } else {
-            tail.next = newNode;
-            newNode.previous = tail;
-            tail = newNode;
+    //Оформляем внутренний класс CustomLinkedList и складываем в него все методы, относящиеся к нему
+    static class CustomLinkedList {
+        public Node<Task> head = null;
+        public Node<Task> tail = null;
+        public void linkLast(Task task){
+            Node<Task> newNode = new Node<>(task);
+            if(head == null){
+                head = tail = newNode;
+                head.previous = null;
+            } else {
+                tail.next = newNode;
+                newNode.previous = tail;
+                tail = newNode;
+            }
+            tail.next = null;
         }
-        tail.next = null;
+
+        public ArrayList<Task> getTasks(){
+            if (head == null){
+                System.out.println("История задач - пуста");
+                return null;
+            } else{
+                ArrayList<Task> viewedTasksArrayList = new ArrayList<>();
+                Node<Task> current = head;
+                while (current != null){
+                    viewedTasksArrayList.add(current.task);
+                    current = current.next;
+                }
+                return viewedTasksArrayList;
+            }
+        }
+
+
+
+        public void removeNode(Node<Task> node){
+            //Удаление из CustomLinkedList
+            //Проверяем, не null ли нам передали, не пуст ли список
+            if (node == null || head == null) {
+                System.out.println("При удалении передан null, либо список пуст, не можем удалить");
+            }
+
+            else {
+                //Проверяем, не head ли надо удалить
+                if (node == head) {
+                    //Если да, то делаем head-ом следующего
+                    head = node.next;
+                }
+                //Проверяем, не tail ли надо удалить
+                if (node == tail) {
+                    //Если да, то делаем head-ом следующего
+                    tail = node.previous;
+                }
+                //Если next у удаляемого - не null, изменим его
+                if (node.next != null){
+                    node.next.previous = node.previous;
+                }
+                //Если previous у удаляемого - не null, изменим его
+                if(node.previous != null){
+                    node.previous.next = node.next;
+                }
+            }
+
+        }
     }
 
-    public ArrayList<Task> getTasks(){
-        if (head == null){
-            System.out.println("История задач - пуста");
-            return null;
-        } else{
-            ArrayList<Task> viewedTasksArrayList = new ArrayList<>();
-            Node<Task> current = head;
-            while (current != null){
-            viewedTasksArrayList.add(current.task);
-            current = current.next;
-            }
-            return viewedTasksArrayList;
-        }
-    }
 
-    public void removeNode(Node<Task> node){
-        //Удаление из CustomLinkedList
-        //Проверяем, не null ли нам передали, не пуст ли список
-        if (node == null || head == null) {
-            System.out.println("При удалении передан null, либо список пуст, не можем удалить");
-        }
 
-        else {
-            //Проверяем, не head ли надо удалить
-            if (node == head) {
-                //Если да, то делаем head-ом следующего
-                head = node.next;
-            }
-            //Проверяем, не tail ли надо удалить
-            if (node == tail) {
-                //Если да, то делаем head-ом следующего
-                tail = node.previous;
-            }
-            //Если next у удаляемого - не null, изменим его
-            if (node.next != null){
-                node.next.previous = node.previous;
-            }
-            //Если previous у удаляемого - не null, изменим его
-            if(node.previous != null){
-                node.previous.next = node.next;
-            }
-        }
 
-    }
     @Override
     public void add(Task task) {
         //Проверяем, есть ли задача в списке
@@ -76,23 +87,23 @@ public class InMemoryHistoryManager implements HistoryManager {
             remove(task.getId());
         }
         //Добавляем задачу в конец нашего CustomLinkedList, формируя из неё узел tail
-        linkLast(task);
+        customLinkedList.linkLast(task);
         //Добавляем узел tail в нашу мапу с историей просмотров
-        tasksViewingHistory.put(task.getId(),tail);
+        tasksViewingHistory.put(task.getId(),customLinkedList.tail);
     }
 
     @Override
     public void remove(int id){
-            //Сначала временно сохраняем Node чтоб после удаления Nod-а из мапы с историей удалить его из Custom LinkedList
-            Node<Task> nodeToBeDeleted = tasksViewingHistory.get(id);
-            //Удаляем из мапы
-            tasksViewingHistory.remove(id);
-            //Удаляем из CustomLinkedList
-            removeNode(nodeToBeDeleted);
+        //Сначала временно сохраняем Node чтоб после удаления Nod-а из мапы с историей удалить его из Custom LinkedList
+        Node<Task> nodeToBeDeleted = tasksViewingHistory.get(id);
+        //Удаляем из мапы
+        tasksViewingHistory.remove(id);
+        //Удаляем из CustomLinkedList
+        customLinkedList.removeNode(nodeToBeDeleted);
     }
     @Override
     public List<Task> getHistory() {
-        return getTasks();
+        return customLinkedList.getTasks();
     }
 
 }
